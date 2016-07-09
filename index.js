1937858c35594f754fdf77a78305d1c4eb024108
Tim Mendoza <timmydoza@gmail.com>
2016-07-09 11:44:01 -0700

    add lib folder

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var readDir = require('fs').readdir;
-var fs = require('fs');
 var join = require('path').join;
 var resolve = require('path').resolve;
 var _ = require('lodash');
+var directoryExists = require('directory_exists');
+var getPayload = require('get_payload');
 var mustache = require('mustache');
 var template = fs.readFileSync(__dirname + '/assets/template.html').toString();
 mustache.parse(template);
@@ -30,51 +30,10 @@ module.exports = function(photoPath, options) {
   if (previewPath) app.use('/downloads', express.static(photoPath));
 
   app.get('/', function(req, res) {
-
-    //Get files in directory
-    readDir(resolve(photoPath), function(err, files) {
-      if (err) throw err;
-      var photoObjects = [];
-
-      files.forEach(function(file) {
-        var ext = file.split('.');
-        var photoObject = {};
-
-        //If file is a JPEG --add more file types
-        if (ext.length > 1 && ext[ext.length - 1] === 'jpg') {
-
-          photoObject.src = join('photos', file);
-          if (thumbPath) photoObject.thumb = join('thumbs', file);
-          if (previewPath) photoObject.downloadUrl = join('downloads', file);
-
-          photoObjects.push(photoObject);
-        }
-      });
-
-      var payload = _.assign({
-          download: true,
-          thumbnail: !!thumbPath
-        },
-        options,
-        {
-          dynamic: true,
-          dynamicEl: photoObjects,
-          closable: false,
-          escKey: false,
-        });
+    getPayload(photoPath, options, function(payload) {
       res.send(mustache.render(template, {title: 'OK', data: JSON.stringify(payload)}));
     });
   });
+  
   return app;
-};
-
-//Utils
-var statSync = require('fs').statSync;
-
-var directoryExists = function(directory) {
-  try {
-    return statSync(resolve(directory)).isDirectory();
-  } catch (e) {
-    return false;
-  }
 };
