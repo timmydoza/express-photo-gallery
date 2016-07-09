58d78bcb5a4491de4ae343eac6a05796d0ee8236
Tim Mendoza <timmydoza@gmail.com>
2016-07-09 12:01:02 -0700

    correct vairable names

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+var fs = require('fs');
 var express = require('express');
-var join = require('path').join;
-var resolve = require('path').resolve;
-var _ = require('lodash');
-var directoryExists = require('directory_exists');
-var getPayload = require('get_payload');
+var directoryExists = require(__dirname + '/lib/directory_exists');
+var getPayload = require(__dirname + '/lib/get_payload');
 var mustache = require('mustache');
 var template = fs.readFileSync(__dirname + '/assets/template.html').toString();
 mustache.parse(template);
@@ -11,29 +9,37 @@ mustache.parse(template);
 module.exports = function(photoPath, options) {
   var app = express.Router();
 
-  var previewPath;
-  var thumbPath;
+  options = options || {};
+
+  var paths = {
+    photos: photoPath,
+    previews: null,
+    thumbs: null
+  };
 
   if (!directoryExists(photoPath)) throw new Error('Must provide valid path for photos');
 
   if (directoryExists(photoPath + '/previews')) {
-    previewPath = photoPath + '/previews';
+    paths.previews = photoPath + '/previews';
   }
 
   if (directoryExists(photoPath + '/thumbs')) {
-    thumbPath = photoPath + '/thumbs';
+    paths.thumbs = photoPath + '/thumbs';
   }
 
   app.use('/', express.static(__dirname + '/assets'));
-  app.use('/photos', express.static(previewPath || photoPath));
-  if (thumbPath) app.use('/thumbs', express.static(thumbPath));
-  if (previewPath) app.use('/downloads', express.static(photoPath));
+  app.use('/photos', express.static(paths.previews || photoPath));
+  if (paths.thumbs) app.use('/thumbs', express.static(paths.thumbs));
+  if (paths.previews) app.use('/downloads', express.static(photoPath));
 
   app.get('/', function(req, res) {
-    getPayload(photoPath, options, function(payload) {
-      res.send(mustache.render(template, {title: 'OK', data: JSON.stringify(payload)}));
+    getPayload(paths, options, function(payload) {
+      res.send(mustache.render(template, {
+        title: options.title || 'Photo Gallery',
+        data: JSON.stringify(payload)
+      }));
     });
   });
-  
+
   return app;
 };
