7c098d8c383ef20bf9474ccf4f487f08649471ba
Tim Mendoza <timmydoza@gmail.com>
2016-07-09 15:57:38 -0700

    remove express dependancy and add router and serve-static

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
-var express = require('express');
+var Router = require('router');
+var static = require('serve-static');
 var directoryExists = require(__dirname + '/lib/directory_exists');
 var getPayload = require(__dirname + '/lib/get_payload');
 var resolveModulePath = require(__dirname + '/lib/resolve_module_path');
@@ -8,7 +9,7 @@ var template = fs.readFileSync(__dirname + '/lib/template.html').toString();
 mustache.parse(template);
 
 module.exports = function(photoPath, options) {
-  var app = express.Router();
+  var app = Router();
 
   options = options || {};
 
@@ -28,10 +29,10 @@ module.exports = function(photoPath, options) {
     paths.thumbs = photoPath + '/thumbs';
   }
 
-  app.use('/lg', express.static(resolveModulePath('lightgallery') + '/dist'));
-  app.use('/photos', express.static(paths.previews || photoPath));
-  if (paths.thumbs) app.use('/thumbs', express.static(paths.thumbs));
-  if (paths.previews) app.use('/downloads', express.static(photoPath));
+  app.use('/lg', static(resolveModulePath('lightgallery') + '/dist'));
+  app.use('/photos', static(paths.previews || photoPath));
+  if (paths.thumbs) app.use('/thumbs', static(paths.thumbs));
+  if (paths.previews) app.use('/downloads', static(photoPath));
 
   app.get('/', function(req, res) {
     getPayload(paths, options, function(payload) {
