1bf6f7ea4c9a6817edb404091c3f754969e130de
maxime <maxime.brochard94@gmail.com>
2020-11-08 17:44:50 +0100

    initial commit for GCS support

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var static = require('serve-static');
 var directoryExists = require('directory-exists').sync;
 var getPayload = require(__dirname + '/lib/get_payload');
 var getS3Payload = require(__dirname + '/lib/get_s3_payload');
+
+var getGCSPayload = require(__dirname + '/lib/get_gcs_payload');
+
 var resolveModulePath = require(__dirname + '/lib/resolve_module_path');
 var mustache = require('mustache');
 var template = fs.readFileSync(__dirname + '/lib/template.html').toString();
@@ -21,14 +24,18 @@ module.exports = function(photoPath, options) {
     thumbs: null,
   };
 
+  //get an element in urlObj wich specify aws or gcs
   var urlObj = url.parse(photoPath);
   var s3 = (urlObj.protocol === 's3:');
+  var gcs = (urlObj.protocol === 'https:' && urlObj.hostname == 'storage.googleapis.com');
+  console.log(urlObj.hostname);
 
-  if (s3) {
-    paths.bucket = urlObj.hostname;
+
+  if (gcs){
+    console.log('urlObj protocol is "' + urlObj.protocol + '" and hostname is "' + urlObj.hostname + '"');
   }
 
-  if (!s3) {
+  if (!gcs) {
 
     if (!directoryExists(photoPath)) throw new Error('Must provide valid path for photos');
 
@@ -53,16 +60,16 @@ module.exports = function(photoPath, options) {
 
   app.get('/', function(req, res) {
 
-    if (s3) {
+    if (gcs) {
 
-      getS3Payload(paths.bucket, options, function(payload) {
+      getGCSPayload(photoPath, options, function(payload) {
         res.send(mustache.render(template, {
           title: options.title || 'Photo Gallery',
           data: JSON.stringify(payload)
-        }));
+        }))
       });
 
-    } else {
+    } else{
 
       getPayload(paths, options, function(payload) {
         res.send(mustache.render(template, {
